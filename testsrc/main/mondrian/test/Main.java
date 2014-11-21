7efe26fd0967cbccddee32641333bd2a654882d1
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2014-11-21 12:47:51 +0300

    MONDRIAN-2229 - unit tests addition fixing

--- testsrc/main/mondrian/test/Main.java
+++ testsrc/main/mondrian/test/Main.java
@@ -33,6 +33,7 @@ import mondrian.util.*;
 import mondrian.xmla.*;
 import mondrian.xmla.impl.DynamicDatasourceXmlaServletTest;
 import mondrian.xmla.test.XmlaTest;
+import mondrian.server.FileRepositoryTest;
 
 import junit.framework.Test;
 import junit.framework.*;
@@ -339,6 +340,7 @@ public class Main extends TestSuite {
             addTest(suite, DeadlockTest.class);
 
             addTest(suite, BlockingHashMapTest.class);
+            addTest(suite, FileRepositoryTest.class);
 
             boolean testNonEmpty = isRunOnce();
             if (!MondrianProperties.instance().EnableNativeNonEmpty.get()) {
