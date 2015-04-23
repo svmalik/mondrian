8805f2ea02f2edf511c257dbdf29aa939e234593
mkambol <mkambol@gmail.com>
2015-04-23 09:43:09 -0400

    [BACKLOG-2384] Changes to better support loading and caching of dimension members.
    1) Introduces a "batch resolution" phase prior to final expression
    resolution which collects child name references and loads them at the
    same time.  This avoids the one-at-a-time SQL queries for each child
    member reference within an expression.  IdBatchResolver is the
    relevant class.
    
    2) To better suppport (1), this change also introduces a new caching
    mechanism for ChildByNameConstraints.  If the child members { [Bob],
    [Joe], [Bill] } have already been cached, MemberCacheHelper is now
    smart enough to retrieve any subset of what has formerly been loaded.
    This new functionanlity only works with ChildByNameConstraint checks,
    since it depends on lookups by name.
    
    3) To better handle small levels (under 300 members), this change also
    adds a check for level cardinality when constructing a constraint in
    SqlConstraintFactory.  In general, any level with fewer than 300
    members will be loaded with a default constraint to avoid repeat
    queries to the dimension table.  There are some exceptions to this,
    for example degenerate dimensions are assumed to be too expensive for
    full table scans, so a Sql constraint is preferred.  This is governed by
    a new property called "LevelPreCacheThreshold", which can be set to
    0 to disable the new behavior.
    
    Conflicts:
    	src/main/mondrian/olap/Query.java
    	src/main/mondrian/rolap/SqlConstraintFactory.java
    	testsrc/main/mondrian/rolap/BatchTestCase.java
    	testsrc/main/mondrian/rolap/NonEmptyTest.java
    	testsrc/main/mondrian/test/Main.java
    	testsrc/main/mondrian/test/NativeSetEvaluationTest.java

--- src/main/mondrian/olap/Query.java
+++ src/main/mondrian/olap/Query.java
@@ -5,10 +5,9 @@
 // You must accept the terms of that agreement to use this software.
 //
 // Copyright (C) 1998-2005 Julian Hyde
-// Copyright (C) 2005-2013 Pentaho and others
+// Copyright (C) 2005-2015 Pentaho and others
 // All Rights Reserved.
 */
-
 package mondrian.olap;
 
 import mondrian.calc.*;
@@ -23,7 +22,7 @@ import mondrian.util.ArrayStack;
 
 import org.apache.commons.collections.collection.CompositeCollection;
 
-import org.olap4j.impl.IdentifierParser;
+import org.olap4j.impl.*;
 import org.olap4j.mdx.IdentifierSegment;
 
 import java.io.PrintWriter;
@@ -40,7 +39,7 @@ import java.util.*;
  * <h3>Query control</h3>
  *
  * <p>Most queries are model citizens, executing quickly (often using cached
- * results from previous queries), but some queries take more time, or more
+ * results from previous queries), but som queries take more time, or more
  * database resources, or more results, than is reasonable. Mondrian offers
  * three ways to control rogue queries:<ul>
  *
@@ -309,7 +308,18 @@ public class Query extends QueryPart {
     public Validator createValidator() {
         return createValidator(
             statement.getSchema().getFunTable(),
-            false);
+            false,
+            new HashMap<QueryPart, QueryPart>());
+    }
+
+
+    public Validator createValidator(
+        Map<QueryPart, QueryPart> resolvedIdentifiers)
+    {
+        return createValidator(
+            statement.getSchema().getFunTable(),
+            false,
+            resolvedIdentifiers);
     }
 
     /**
@@ -328,7 +338,21 @@ public class Query extends QueryPart {
         return new QueryValidator(
             functionTable,
             alwaysResolveFunDef,
-            Query.this);
+            Query.this,
+            new HashMap<QueryPart, QueryPart>());
+    }
+
+
+    public Validator createValidator(
+        FunTable functionTable,
+        boolean alwaysResolveFunDef,
+        Map<QueryPart, QueryPart> resolvedIdentifiers)
+    {
+        return new QueryValidator(
+            functionTable,
+            alwaysResolveFunDef,
+            Query.this,
+            resolvedIdentifiers);
     }
 
     /**
@@ -452,7 +476,12 @@ public class Query extends QueryPart {
      * Same as {@link #resolve()} with externally defined parameters.
      */
     protected void resolve(List<Parameter> parameters) {
-        final Validator validator = createValidator();
+        // Before commencing validation, create all calculated members
+        // and calculated sets
+        createFormulaElements();
+        Map<QueryPart, QueryPart> resolvedIdentifiers =
+            new IdBatchResolver(this).resolve();
+        final Validator validator = createValidator(resolvedIdentifiers);
         resolve(validator, parameters); // resolve self and children
         // Create a dummy result so we can use its evaluator
         final Evaluator evaluator = RolapUtil.createEvaluator(statement);
@@ -460,6 +489,17 @@ public class Query extends QueryPart {
             createCompiler(
                 evaluator, validator, Collections.singletonList(resultStyle));
         compile(compiler);
+    }
+
+    private void createFormulaElements() {
+        if (formulas != null) {
+            // Resolving of formulas should be done in two parts
+            // because formulas might depend on each other, so all calculated
+            // mdx elements have to be defined during resolve.
+            for (Formula formula : formulas) {
+                formula.createElement(this);
+            }
+        }
     }
 
     /**
@@ -540,17 +580,6 @@ public class Query extends QueryPart {
     }
 
     protected void resolve(Validator validator, List<Parameter> params) {
-        // Before commencing validation, create all calculated members,
-        // calculated sets, and parameters.
-        if (formulas != null) {
-            // Resolving of formulas should be done in two parts
-            // because formulas might depend on each other, so all calculated
-            // mdx elements have to be defined during resolve.
-            for (Formula formula : formulas) {
-                formula.createElement(validator.getQuery());
-            }
-        }
-
         // Register all parameters.
         parameters.clear();
         parametersByName.clear();
@@ -1530,7 +1559,7 @@ public class Query extends QueryPart {
                 if (member == null) {
                     continue;
                 }
-                if (!match(member, nameParts)) {
+                if (!Util.matches(member, nameParts)) {
                     continue;
                 }
                 if (!query.getConnection().getRole().canAccess(member)) {
@@ -1541,36 +1570,7 @@ public class Query extends QueryPart {
             return null;
         }
 
-        private static boolean match(
-            Member member, List<Id.Segment> nameParts)
-        {
-            if (Util.equalName(Util.implode(nameParts),
-                member.getUniqueName()))
-            {
-                // exact match
-                return true;
-            }
-            Id.Segment segment = nameParts.get(nameParts.size() - 1);
-            while (member.getParentMember() != null) {
-                if (!segment.matches(member.getName())) {
-                    return false;
-                }
-                member = member.getParentMember();
-                nameParts = nameParts.subList(0, nameParts.size() - 1);
-                segment = nameParts.get(nameParts.size() - 1);
-            }
-            if (segment.matches(member.getName())) {
-                return Util.equalName(
-                    member.getHierarchy().getUniqueName(),
-                    Util.implode(nameParts.subList(0, nameParts.size() - 1)));
-            } else if (member.isAll()) {
-                return Util.equalName(
-                    member.getHierarchy().getUniqueName(),
-                    Util.implode(nameParts));
-            } else {
-                return false;
-            }
-        }
+
 
         public List<Member> getCalculatedMembers(Hierarchy hierarchy) {
             List<Member> result = new ArrayList<Member>();
@@ -1799,9 +1799,10 @@ public class Query extends QueryPart {
          * @param query Query
          */
         public QueryValidator(
-            FunTable functionTable, boolean alwaysResolveFunDef, Query query)
+            FunTable functionTable, boolean alwaysResolveFunDef, Query query,
+            Map<QueryPart, QueryPart> resolvedIdentifiers)
         {
-            super(functionTable);
+            super(functionTable, resolvedIdentifiers);
             this.alwaysResolveFunDef = alwaysResolveFunDef;
             this.query = query;
             this.schemaReader = new ScopedSchemaReader(this, true);
