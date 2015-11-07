3298fb71bcbb2c800ba02794ac9e1eeb54d33406
svmalik <sergei.malik@gmail.com>
2015-11-06 18:55:44 -0800

    NonEmpty native evaluation with measures from multiple cubes.

--- src/main/mondrian/olap/fun/NonEmptyFunDef.java
+++ src/main/mondrian/olap/fun/NonEmptyFunDef.java
@@ -18,18 +18,30 @@ import mondrian.calc.TupleCursor;
 import mondrian.calc.TupleIterable;
 import mondrian.calc.TupleList;
 import mondrian.calc.impl.AbstractListCalc;
+import mondrian.mdx.MemberExpr;
 import mondrian.mdx.ResolvedFunCall;
 import mondrian.olap.Evaluator;
 import mondrian.olap.Exp;
 import mondrian.olap.FunDef;
 import mondrian.olap.Hierarchy;
 import mondrian.olap.Member;
+import mondrian.olap.MondrianProperties;
 import mondrian.olap.NativeEvaluator;
+import mondrian.olap.Syntax;
+import mondrian.olap.Validator;
 import mondrian.olap.type.MemberType;
+import mondrian.olap.type.SetType;
 import mondrian.olap.type.TupleType;
+import mondrian.rolap.RolapCube;
+import mondrian.rolap.RolapStoredMeasure;
+import mondrian.server.Locus;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class NonEmptyFunDef extends FunDefBase {
     static final MultiResolver NonEmptyResolver =
@@ -73,22 +85,106 @@ public class NonEmptyFunDef extends FunDefBase {
             public TupleList evaluateList(Evaluator evaluator) {
                 final int savepoint = evaluator.savepoint();
                  try {
+                     TupleIterable setIterable = null;
                     // empty args can be stripped at the source for some cases
                     // when is it ok to set nonempty?
 
                     // attempt native
                     NativeEvaluator nativeEvaluator =
-                        evaluator.getSchemaReader().getNativeSetEvaluator(
-                            call.getFunDef(), call.getArgs(), evaluator, this);
+                        getNativeEvaluator(evaluator, call, this);
                     if (nativeEvaluator != null) {
                         evaluator.restore(savepoint);
                         return
                             (TupleList) nativeEvaluator.execute(
                                 ResultStyle.LIST);
+                    } else if (evaluator.nativeEnabled()
+                            && MondrianProperties.instance().EnableNativeNonEmptyFunctionDifferentCubes.get())
+                    {
+                        // This is to address a case when there are measures from different
+                        // measure groups in the second arg. The idea is to group measures
+                        // and run each measure group separately, and then union results.
+                        final boolean tryNonNative =
+                            MondrianProperties.instance().EnableNativeNonEmptyFunctionDifferentCubesAndNonNative.get();
+                        Exp[] args = call.getArgs();
+                        if (args.length == 2) {
+                            if (args[1] instanceof ResolvedFunCall) {
+                                ResolvedFunCall call = (ResolvedFunCall)args[1];
+                                while (("{}".equals(call.getFunName()) || "()".equals(call.getFunName()))
+                                    && call.getArgCount() == 1
+                                    && call.getArg(0) instanceof ResolvedFunCall)
+                                {
+                                    call = (ResolvedFunCall)call.getArg(0);
+                                }
+                                Map<RolapCube, List<Exp>> measureMap =
+                                    new HashMap<RolapCube, List<Exp>>();
+                                for (Exp arg1 : call.getArgs()) {
+                                    if (arg1 instanceof MemberExpr) {
+                                        Member member = ((MemberExpr)arg1).getMember();
+                                        RolapCube cube = null;
+                                        if (member instanceof RolapStoredMeasure) {
+                                            cube = ((RolapStoredMeasure) member).getCube();
+                                        }
+                                        List<Exp> measures = measureMap.get(cube);
+                                        if (measures == null) {
+                                            measures = new ArrayList<Exp>();
+                                            measureMap.put(cube, measures);
+                                        }
+                                        measures.add(arg1);
+                                    }
+                                }
+
+                                List<TupleList> nonEmptyTuples;
+                                if (measureMap.keySet().size() > 1) {
+                                    nonEmptyTuples = new ArrayList<TupleList>();
+                                    ExpCompiler expCompiler = evaluator.getQuery().createCompiler();
+                                    Validator validator = expCompiler.getValidator();
+                                    for (List<Exp> measures : measureMap.values()) {
+                                        Exp[] setArgs = measures.toArray(new Exp[measures.size()]);
+                                        FunDef set = validator.getDef(setArgs, "{}", Syntax.Braces);
+                                        Exp setExp = new ResolvedFunCall(set, setArgs, new SetType(MemberType.Unknown));
+                                        Exp[] newArgs = new Exp[] { args[0], setExp};
+                                        FunDef nonEmptyFun = validator.getDef(newArgs, "NonEmpty", Syntax.Function);
+                                        ResolvedFunCall nonEmpty = new ResolvedFunCall(nonEmptyFun, newArgs, new SetType(MemberType.Unknown));
+                                        int save = evaluator.savepoint();
+                                        nativeEvaluator = getNativeEvaluator(evaluator, nonEmpty, this);
+                                        evaluator.restore(save);
+                                        if (nativeEvaluator != null) {
+                                            nonEmptyTuples.add((TupleList) nativeEvaluator.execute(ResultStyle.LIST));
+                                        } else {
+                                            if (!tryNonNative) {
+                                                nonEmptyTuples = null;
+                                                break;
+                                            }
+                                            ListCalc arg2Calc2 = expCompiler.compileList(setExp);
+                                            if (setIterable == null) {
+                                                setIterable = arg1Calc.evaluateIterable(evaluator);
+                                            }
+                                            TupleIterable auxIterable2 = arg2Calc2.evaluateIterable(evaluator);
+                                            nonEmptyTuples.add(
+                                                auxIterable2 == null
+                                                    ? nonEmpty(evaluator, setIterable)
+                                                    : nonEmpty(evaluator, setIterable, auxIterable2));
+                                        }
+                                    }
+                                    if (nonEmptyTuples != null && !nonEmptyTuples.isEmpty()) {
+                                        TupleList result = null;
+                                        Set<List<Member>> added = new HashSet<List<Member>>();
+                                        for (TupleList tuples : nonEmptyTuples) {
+                                            if (result == null) {
+                                                result = TupleCollections.createList(tuples.getArity());
+                                            }
+                                            FunUtil.addUnique(result, tuples, added);
+                                        }
+                                        return result;
+                                    }
+                                }
+                            }
+                        }
                     }
 
-                    final TupleIterable setIterable =
-                        arg1Calc.evaluateIterable(evaluator);
+                     if (setIterable == null) {
+                         setIterable = arg1Calc.evaluateIterable(evaluator);
+                     }
                     TupleIterable auxIterable =
                         arg2Calc != null
                             ? arg2Calc.evaluateIterable(evaluator)
@@ -100,6 +196,12 @@ public class NonEmptyFunDef extends FunDefBase {
                 } finally {
                     evaluator.restore(savepoint);
                 }
+            }
+
+            private NativeEvaluator getNativeEvaluator(Evaluator evaluator, ResolvedFunCall call, Calc calc) {
+                return
+                    evaluator.getSchemaReader().getNativeSetEvaluator(
+                        call.getFunDef(), call.getArgs(), evaluator, calc);
             }
         };
     }
@@ -130,6 +232,9 @@ public class NonEmptyFunDef extends FunDefBase {
         TupleCursor auxCursor = null;
         final Member[] currentMembers = new Member[arity];
 
+        final int checkCancelPeriod = MondrianProperties.instance().CancelPhaseInterval.get();
+        int rowCount = 0;
+
         // "crossjoin" iterables and check for nonemptyness
         // only the first tuple is returned
         while (mainCursor.forward()) {
@@ -151,6 +256,11 @@ public class NonEmptyFunDef extends FunDefBase {
             }
             if (isNonEmpty) {
                 result.add(mainCursor.current());
+            }
+
+            rowCount++;
+            if (checkCancelPeriod > 0 && rowCount % checkCancelPeriod == 0) {
+                Locus.peek().execution.checkCancelOrTimeout();
             }
         }
         return result;
