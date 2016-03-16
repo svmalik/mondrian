045d1029068cae453ddb3219ced3ccef0bb99352
svmalik <sergei.malik@gmail.com>
2016-03-15 19:37:01 -0700

    [test] Fixing Teradata support in AggregationOnDistinctCountMeasuresTest.

--- testsrc/main/mondrian/rolap/agg/AggregationOnDistinctCountMeasuresTest.java
+++ testsrc/main/mondrian/rolap/agg/AggregationOnDistinctCountMeasuresTest.java
@@ -871,7 +871,8 @@ public class AggregationOnDistinctCountMeasuresTest extends BatchTestCase {
 
         String oraTeraSqlForDistinctCountAgg =
             "select \"time_by_day\".\"the_year\" as \"c0\", "
-            + "count(distinct \"sales_fact_1997\".\"customer_id\") as \"m0\" "
+            + "sum(\"sales_fact_1997\".\"unit_sales\") as \"m0\", "
+            + "count(distinct \"sales_fact_1997\".\"customer_id\") as \"m1\" "
             + "from \"time_by_day\" =as= \"time_by_day\", "
             + "\"sales_fact_1997\" =as= \"sales_fact_1997\", \"store\" =as= \"store\" "
             + "where \"sales_fact_1997\".\"time_id\" = \"time_by_day\".\"time_id\" "
