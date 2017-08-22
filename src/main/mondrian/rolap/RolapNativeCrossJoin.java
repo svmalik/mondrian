9ea09e115e4d9b9da74369dd4315abe0fe324631
Stanislau_Strelchanka <stanislau_strelchanka@epam.com>
2017-08-22 14:42:01 +0300

    Revert "[MONDRIAN-2561] Crossjoin results differs between sql and mondrian when suppress zeros is on"
    # Conflicts:
    #	src/main/mondrian/rolap/RolapNativeCrossJoin.java
    #	testsrc/main/mondrian/test/BasicSteelWheelsQueryTest.java

--- src/main/mondrian/rolap/RolapNativeCrossJoin.java
+++ src/main/mondrian/rolap/RolapNativeCrossJoin.java
@@ -5,7 +5,7 @@
 // You must accept the terms of that agreement to use this software.
 //
 // Copyright (C) 2004-2005 TONBELLER AG
-// Copyright (C) 2006-2017 Pentaho
+// Copyright (C) 2006-2015 Pentaho
 // All Rights Reserved.
 */
 package mondrian.rolap;
@@ -200,16 +200,6 @@ public class RolapNativeCrossJoin extends RolapNativeSet {
             }
         }
 
-        // Use the combined CrossJoinArg for the tuple constraint,
-        // which will be translated to the SQL WHERE clause.
-        CrossJoinArg[] cargs = combineArgs(allArgs);
-        CrossJoinArg[] constrainArgs;
-        if (safeToConstrainByOtherAxes(fun)) {
-            constrainArgs = buildArgs(evaluator, cargs);
-        } else {
-            constrainArgs = cargs;
-        }
-
         if (SqlConstraintUtils.measuresConflictWithMembers(
                 cjArgs, evaluator))
         {
@@ -263,11 +253,15 @@ public class RolapNativeCrossJoin extends RolapNativeSet {
         try {
             overrideContext(evaluator, cjArgs, null);
 
+            // Use the combined CrossJoinArg for the tuple constraint,
+            // which will be translated to the SQL WHERE clause.
+            CrossJoinArg[] cargs = combineArgs(allArgs);
+
             // Now construct the TupleConstraint that contains both the CJ
             // dimensions and the additional filter on them. It will make a
             // copy of the evaluator.
             TupleConstraint constraint =
-                buildConstraint(evaluator, fun, constrainArgs);
+                buildConstraint(evaluator, fun, cargs);
             // Use the just the CJ CrossJoiArg for the evaluator context,
             // which will be translated to select list in sql.
             final SchemaReader schemaReader = evaluator.getSchemaReader();
@@ -305,9 +299,15 @@ public class RolapNativeCrossJoin extends RolapNativeSet {
     private TupleConstraint buildConstraint(
         final RolapEvaluator evaluator,
         final FunDef fun,
-        final CrossJoinArg[] constraintArgs)
+        final CrossJoinArg[] cargs)
     {
-        return new NonEmptyCrossJoinConstraint(constraintArgs, evaluator);
+        CrossJoinArg[] myArgs;
+        if (safeToConstrainByOtherAxes(fun)) {
+            myArgs = buildArgs(evaluator, cargs);
+        } else {
+            myArgs = cargs;
+        }
+        return new NonEmptyCrossJoinConstraint(myArgs, evaluator);
     }
 
     private CrossJoinArg[] buildArgs(
